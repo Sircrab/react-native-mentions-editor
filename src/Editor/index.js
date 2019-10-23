f4c1ae3cbc8e6697e5aa92550efc01f7e5d0a41d
Sircrab <rovalfer@gmail.com>
2019-10-23 12:25:04 -0300

    Adds callback for return on mentionMode (#4)
    * keyboard changes
    
    * comments
    
    * more comments

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -16,6 +16,8 @@ import MentionList from '../MentionList';
 
 export class Editor extends React.Component {
 
+    skipNextInput = false;
+
     constructor(props) {
         super(props);
         this.mentionsMap = new Map();
@@ -359,6 +361,10 @@ export class Editor extends React.Component {
     }
 
     onChange = (inputText, fromAtBtn) => {
+        if (this.skipNextInput) {
+          this.skipNextInput = false;
+          return;
+        }
         let text = inputText;
         const prevText = this.state.inputText;
         let selection = { ...this.state.selection };
@@ -473,6 +479,13 @@ export class Editor extends React.Component {
         }
     }
 
+    onKeyPress = event => {
+      if (this.state.isTrackingStarted && event.nativeEvent.key === 'Enter' && this.props.onReturnPressed) {
+        this.props.onReturnPressed();
+        this.skipNextInput = true;
+      }
+    }
+
     render() {
         const { props, state } = this;
         const {editorStyles = {}} = props;
@@ -517,6 +530,7 @@ export class Editor extends React.Component {
                                 name={'message'}
                                 value={state.inputText}
                                 onBlur={props.toggleEditor}
+                                onKeyPress={this.onKeyPress}
                                 onChangeText={this.onChange}
                                 selection={this.state.selection}
                                 selectionColor="rgba(0, 0, 0, 0.5)"
